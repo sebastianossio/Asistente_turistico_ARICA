9ab0a558405220e8b556f36594e88fb808585de5
sebastianossio <sebastian.ossio.araya@alumnos.uta.cl>
2025-11-09 23:07:06 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,6 @@
 import streamlit as st
 from openai import OpenAI
 import streamlit as st
-pip install folium
-pip install streamlit pandas folium geopy reportlab googletrans==4.0.0-rc1
 import pandas as pd
 import folium
 from folium import Popup, PolyLine
